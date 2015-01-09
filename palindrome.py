0ff32773ccdf15d67be611f34f89577aa7eab26c
blaklaybul <michaelahirsch@gmail.com>
2015-01-09 17:35:04 -0500

    π approxmiation

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,12 @@
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
 def is_palindrome(word):       
 	if (len(word)==1):
 	    return "yep"
@@ -7,18 +16,10 @@ def is_palindrome(word):
 	    else:
 	        return "no way!"
 	elif(len(word) > 2):
-		if(first(word) == last(word)):
-			newword = middle(word)
-			return is_palindrome(newword)
-		else:
-			"not a palindrome!"
-
-
-def first(word):
-    return word[0]
-
-def last(word):
-    return word[-1]
-
-def middle(word):
-    return word[1:-1]
+		print word
+		newword	= middle(word)
+		while(len(newword) > 1):
+			if(first(word) == last(word)):
+				is_palindrome(newword)
+			else:
+				return "not a palindrome!"
