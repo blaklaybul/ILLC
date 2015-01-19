33fc5a8c225d87361f5f861f38c6088020b3acd4
blaklaybul <michaelahirsch@gmail.com>
2015-01-19 20:57:17 +0100

    new plots

--- MLB/MLB_randomforest.R
+++ MLB/MLB_randomforest.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 teams = read.csv("MLBTeams.csv", header = TRUE)
 
 teams$Ghome <- NULL
@@ -5,5 +7,7 @@ teams$IPouts <- NULL
 
 names(teams) <- c("year","league", "teamID", "franchiseID", "rank", "games", "wins", "losses","pennant", "worldSeries", "runs", "atBats", "hits","doubles", "triples", "homeruns","walks", "strikeouts", "stolenBases", "runsAgainst","earnedRuns","era", "completeGames", "shutouts","saves", "hitsAllowed", "homerunsAllowed", "walksAllowed", "Ks","errors", "doublePlays", "fieldingPercentage","teamName", "park", "attendance")
 
-# there should be no null values in this data fram
+# there should be no null values in this data frame
+# p.new <- ggplot(teams, aes(x=teams$wins, y=teams$attendance, col=teams$worldSeries)) + geom_point(size=3,alpha=0.8)+ xlab("wins") +  ylab("attendance") + scale_y_continuous(labels = comma)
+
 
