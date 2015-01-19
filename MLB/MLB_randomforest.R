bb39954af6eb24fa899bd93fec7acb98fae58668
blaklaybul <michaelahirsch@gmail.com>
2015-01-19 20:21:51 +0100

    renaming columns

--- MLB/MLB_randomforest.R
+++ MLB/MLB_randomforest.R
@@ -1,2 +1,9 @@
 teams = read.csv("MLBTeams.csv", header = TRUE)
 
+teams$Ghome <- NULL
+teams$IPouts <- NULL
+
+names(teams) <- c("year","league", "teamID", "franchiseID", "rank", "games", "wins", "losses","pennant", "worldSeries", "runs", "atBats", "hits","doubles", "triples", "homeruns","walks", "strikeouts", "stolenBases", "runsAgainst","earnedRuns","era", "completeGames", "shutouts","saves", "hitsAllowed", "homerunsAllowed", "walksAllowed", "Ks","errors", "doublePlays", "fieldingPercentage","teamName", "park", "attendance")
+
+# there should be no null values in this data fram
+
