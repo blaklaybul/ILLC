4f2c585ce03c28f43341bed2ee8e65460c6b6b2c
blaklaybul <michaelahirsch@gmail.com>
2015-01-19 23:49:09 +0100

    such random such forest

--- MLB/MLB_randomforest.R
+++ MLB/MLB_randomforest.R
@@ -1,23 +1,33 @@
-library(ggplot2)
-library(randomForest)
-library(trees)
+require(ggplot2)
+require(randomForest)
+require(tree)
 
 teams = read.csv("MLBTeams.csv", header = TRUE)
 
-attach(teams)
-
 names(teams) <- c("year","league", "franchiseID", "rank", "games", "wins", "losses","pennant", "worldSeries", "runs", "atBats", "hits","doubles", "triples", "homeruns","walks", "strikeouts", "stolenBases", "runsAgainst","earnedRuns","era","saves", "hitsAllowed", "homerunsAllowed", "walksAllowed", "Ks","errors", "doublePlays", "fieldingPercentage", "attendance")
 
-rank <- NULL
-winningPCT <- teams$wins/teams$games
-wins <- NULL
-losses <- NULL
-franchiseID <- as.character(franchiseID)
-year <- as.character(year)
-franchiseID <- do.call(paste,c(teams[c("franchiseID","year")]))
-league <- NULL
-year <- NULL
+teams$franchiseID <- as.character(teams$franchiseID)
+teams$year <- as.character(teams$year)
+teams$franchiseID <- do.call(paste,c(teams[c("franchiseID","year")]))
+teams$winningPCT = teams$wins/teams$games
+teams$rank <- NULL
+teams$league <- NULL
+teams$year <- NULL
+teams$wins <- NULL
+teams$losses <- NULL
+
 # there should be no null values in this data frame
 # p.new <- ggplot(teams, aes(x=teams$wins, y=teams$attendance, col=teams$worldSeries)) + geom_point(size=3,alpha=0.8)+ xlab("wins") +  ylab("attendance") + scale_y_continuous(labels = comma)
 
+rf.teams=randomForest(worldSeries~.-franchiseID, data=teams, subset=train)
+rf.teams=randomForest(winningPCT~.-franchiseID-worldSeries, data=teams, subset=train)
 
+for(mtry in 1:26){
+  fit=randomForest(winningPCT~.-franchiseID-worldSeries, data=teams, subset=train, mtry=mtry,ntree=400)
+  oob.err[mtry]=fit$mse[400]
+  pred = predict(fit,teams[-train,])
+  test.err[mtry]=with(teams[-train,],mean((winningPCT-pred)^2))
+  cat(mtry," ")                      
+}
+
+matplot(1:mtry,cbind(test.err,oob.err),pch=19,col=c("red","blue"),type="b", ylab="mean sq error")
