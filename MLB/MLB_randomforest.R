cd340c35a19561fd8905a4d6b8bb5200cdd4aef4
blaklaybul <michaelahirsch@gmail.com>
2015-01-19 22:15:47 +0100

    shits wonkys

--- MLB/MLB_randomforest.R
+++ MLB/MLB_randomforest.R
@@ -1,11 +1,13 @@
 library(ggplot2)
+library(randomForest)
+library(trees)
 
 teams = read.csv("MLBTeams.csv", header = TRUE)
 
-teams$Ghome <- NULL
-teams$IPouts <- NULL
+names(teams) <- c("year","league", "franchiseID", "rank", "games", "wins", "losses","pennant", "worldSeries", "runs", "atBats", "hits","doubles", "triples", "homeruns","walks", "strikeouts", "stolenBases", "runsAgainst","earnedRuns","era","saves", "hitsAllowed", "homerunsAllowed", "walksAllowed", "Ks","errors", "doublePlays", "fieldingPercentage", "attendance")
 
-names(teams) <- c("year","league", "teamID", "franchiseID", "rank", "games", "wins", "losses","pennant", "worldSeries", "runs", "atBats", "hits","doubles", "triples", "homeruns","walks", "strikeouts", "stolenBases", "runsAgainst","earnedRuns","era", "completeGames", "shutouts","saves", "hitsAllowed", "homerunsAllowed", "walksAllowed", "Ks","errors", "doublePlays", "fieldingPercentage","teamName", "park", "attendance")
+teams$franchiseID <- as.character(teams$franchiseID)
+teams$league <- as.character(teams$league)
 
 # there should be no null values in this data frame
 # p.new <- ggplot(teams, aes(x=teams$wins, y=teams$attendance, col=teams$worldSeries)) + geom_point(size=3,alpha=0.8)+ xlab("wins") +  ylab("attendance") + scale_y_continuous(labels = comma)
