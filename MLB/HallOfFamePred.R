76c732e210c28083dd8afde235100df84223cc12
blaklaybul <michaelahirsch@gmail.com>
2015-01-24 18:14:10 +0100

    lots of work

--- MLB/HallOfFamePred.R
+++ MLB/HallOfFamePred.R
@@ -5,6 +5,9 @@ require(XML)
 require(plyr)
 require(stringr)
 require(tree)
+require(rpart)
+require(rpart.plot)
+require(party) 
 
 batting <- data.frame(read.csv("Batting.csv", header=TRUE))
 hof <- data.frame(read.csv("HallOfFame.csv", header=TRUE))
@@ -24,29 +27,30 @@ batting[is.na(batting)] <- 0
 battingagg <- aggregate(.~playerID, batting, sum)
 HallOfFame <-  merge(x=battingagg,y=hof,by=c("playerID"))
 
-summary(HallOfFame)
-summary(HallOfFame)
 HallOfFame <- HallOfFame[HallOfFame$votedBy =="BBWAA",]
+HallOfFame <- HallOfFame[HallOfFame$RBI > 300,] #we only want hitters
 HallOfFame$votedBy <- NULL
 HallOfFame$ballots <- NULL
 HallOfFame$needed <- NULL
 HallOfFame$votes <- NULL
 HallOfFame$category <- NULL
 HallOfFame$needed_note <- NULL
+HallOfFame$G_old <- NULL
+HallOfFame$yearid <- NULL
+HallOfFame$playerID = as.character(HallOfFame$playerID)
+
+HallOfFame[is.na(HallOfFame$inducted),c("inducted")] <- "N"
 
 HallOfFame$inducted <- ifelse(HallOfFame$inducted == "Y", 1, 0)
-
 HallOfFame <- aggregate(.~playerID,HallOfFame, max)
 
 HallOfFame$inducted <- as.factor(HallOfFame$inducted)
-HallOfFame$playerID = as.character(HallOfFame$playerID)
 
 ##only want batters... this was taken from baseball reference
-HallOfFame <- HallOfFame[HallOfFame$H > 700,]
+# HallOfFame <- HallOfFame[HallOfFame$H > 700,]
 
 #this should be ok.
 
-summary(HallOfFame)
 
 #create one decision tree based on all the data. this will overfit of course
 tree.hof = tree(inducted~.-playerID, data=HallOfFame)
@@ -54,15 +58,18 @@ summary(tree.hof)
 
 ##looking at the tree, this is very bushy, with 22 terminal nodes
 plot(tree.hof)
-text(tree.hof)
+text(tree.hof, pretty=TRUE)
+
+prp(tree.hof)
 
 #lets split to training and test
 set.seed(919)
 
 train=sample(1:nrow(HallOfFame), 400)
 
-tree.hof=tree(inducted~.-playerID-yearid, HallOfFame,subset=train)
+tree.hof=tree(inducted~.-playerID, HallOfFame,subset=train)
 
+summary(tree.hof)
 plot(tree.hof)
 text(tree.hof)
 
@@ -71,7 +78,7 @@ tree.pred=predict(tree.hof,HallOfFame[-train,],type="class")
 with(HallOfFame[-train,],table(tree.pred,inducted))
 
 #what was predicted correctly
-(216+14)/(216+14+12+16)
+(203+18)/(203+18+19+11)
 
 #lets do cross validation to prune the tree.
 cv.hof = cv.tree(tree.hof,FUN=prune.misclass)
@@ -80,7 +87,7 @@ plot(cv.hof)
 
 ##bottoms out around7, lets prune our tree
 
-prune.hof = prune.misclass(tree.hof, best = 7)
+prune.hof = prune.misclass(tree.hof, best = 6)
 
 plot(prune.hof);text(prune.hof,pretty=0)
 
@@ -94,3 +101,11 @@ with(HallOfFame[-train,],table(tree.pred,inducted))
 
 
 (211+15)/(211+15+11+11)
+
+
+hof.tree=tree(inducted~H+RBI, data=HallOfFame)
+plot(hof.tree)
+plot(HallOfFame$H,HallOfFame$RBI,col=HallOfFame$inducted)
+partition.tree(hof.tree, add = TRUE)
+
+
